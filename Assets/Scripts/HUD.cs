f49ffee26acecd69a93230be855a004affd796e4
T-Terra <gabst138@gmail.com>
2024-10-26 16:41:33 -0300

    feat: add HUD script controller

---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,38 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    public static HUD Instance {get; private set;}
+
+    public Slider xp;
+    public TMP_Text souls;
+    public TMP_Text metersDistance;
+    public GameObject PowerUpScreem;
+
+    private void Awake() {
+        if(Instance != null) {
+            Destroy(gameObject);
+        } else {
+            Instance = this;
+        }
+    }
+
+    public void ExpHandler( float points = 10 ) {
+        if(xp.value == xp.maxValue) {
+            PowerUpScreem.SetActive(true);
+            xp.value = 0;
+        }
+        xp.value = points;
+    }
+
+    public void MetersHandler( int RateMeters = 10 ) {
+        float meters = Time.time;
+        metersDistance.text = (meters * RateMeters).ToString("F0") + "M";
+    }
+
+    public void SoulsHandler( int soulsColleted = 0) {
+        souls.text = soulsColleted.ToString();
+    }
+}
