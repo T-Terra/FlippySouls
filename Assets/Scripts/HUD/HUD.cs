ccff616e757f09b304680c0c194cb0c5de606a37
T-Terra <gabst138@gmail.com>
2024-10-27 21:03:29 -0300

    fix: remove time scale EXPHandler

--- Assets/Scripts/HUD/HUD.cs
+++ Assets/Scripts/HUD/HUD.cs
@@ -32,7 +32,7 @@ public class HUD : MonoBehaviour
 
     public void ExpHandler( float points = 10 ) {
         if(xp.value == xp.maxValue) {
-            Time.timeScale = 0;
+            //Time.timeScale = 0;
             //PowerUpScreen.SetActive(true);
             xp.value = 0;
         }
